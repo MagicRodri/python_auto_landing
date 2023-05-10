67ae96ad16d29b93fc9c33c419fe31ce748a4977
Rodrigue Roland CODO <codorol22@gmail.com>
2023-05-10 21:39:10 +0500

    Update landing_target.py

--- src/landing_target.py
+++ src/landing_target.py
@@ -3,10 +3,10 @@ import math
 from pathlib import Path
 import time
 
-import time
 import numpy as np
-from aruco_tracker import ArucoSingleTracker
 from pymavlink import mavutil
+
+from aruco_tracker import ArucoSingleTracker
 from utils import play_buzzer_tune,send_altitude,marker_position_to_angle,send_landing_target
 
 def camera_to_target(x_cam:float, y_cam:float):
@@ -14,7 +14,7 @@ def camera_to_target(x_cam:float, y_cam:float):
     return (x_target, y_target) # target position with respect to camera
 
 def main():
-
+    logging.basicConfig(level=logging.INFO)
     id_to_find = 1
     marker_size = 10  # [cm]
     freq_send       = 15 #- Hz
@@ -37,10 +37,9 @@ def main():
     master.wait_heartbeat()
     
     while True:
-        logging.basicConfig(level=logging.INFO)
         marker_found, x_cm, y_cm, z_cm = aruco_tracker.track(loop=False,verbose=False)
         if marker_found:
-            play_buzzer_tune(master,"T200O2L1A#") # constantly play tune when marker found
+            play_buzzer_tune(master,"T200O2L1A#") # continously play tune when marker found
             x_cm, y_cm = camera_to_target(x_cm, y_cm)
             z_cm = round(z_cm,2)
             logging.info(f"Marker found at ({x_cm}, {y_cm}, {z_cm})")
